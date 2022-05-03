f0db7582eb7081ec1134cd23b8e43ffd4419b2e6
c3t4r4 <neocetara@hotmail.com>
2022-05-03 18:25:12 -0300

    Finish Filter IMSI

--- app/Http/Controllers/IMSIController.php
+++ app/Http/Controllers/IMSIController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Imsi;
 use App\Models\Located;
+use App\Models\Timsi;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\DB;
@@ -10,10 +12,25 @@ use Illuminate\Support\Facades\DB;
 class IMSIController extends Controller
 {
     public function index(Request $request){
-        $locateds = Located::allItens()->get();
+        $locateds = Located::query()
+            ->with('imsi')
+            ->with('timsi')
+            ->when($request->search, function ($query, $search){
+                $imsiIDS = Imsi::where('imsi', 'like', "%${search}%")->get('id')->toArray();
+                $timsiIDS = Timsi::where('timsi', 'like', "%${search}%")->get('id')->toArray();
+
+                $query->where(function($query) use($imsiIDS) {
+                    $query->whereIn('imsi_id', $imsiIDS);
+                });
+                $query->orWhere(function($query) use($timsiIDS) {
+                    $query->whereIn('timsi_id', $timsiIDS);
+                });
+            })
+            ->get();
         
         return Inertia::render('IMSI/index', [
-            "locateds" => $locateds
+            "locateds" => $locateds,
+            "filters" => $request->only(['search']),
         ]);
     }
 }
